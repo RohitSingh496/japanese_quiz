37b91106e0bd96360140b0ec6b9f9bad79f4bbdd
Rohit <sawn@pop-os>
2022-11-29 18:05:54 +0530

    sorted some files

--- genAnswers.sh
+++ genAnswers.sh
@@ -3,7 +3,7 @@
 cat imdb.csv | ./csv2tab > imdb.tsv
 
 #Ans-1
-awk -F'\t' '$1>8.5 {print}' imdb.tsv > ans1.tsv
+awk -F'\t' '$1>8.5 {print}' imdb.tsv > ./assets/answers/ans1.tsv
 
 #Ans-2
 awk -F'\t' '{print $4}' imdb.tsv | sort | uniq > genrelist
@@ -25,13 +25,13 @@ mapfile -t avgR < avgRatings.txt
 mapfile -t avgD < avgDurations.txt
 mapfile -t genre < genrelist
 
-echo "genre|noOfMovies|avgRatings|avgDurations" > ans2_3.txt
-for i in {0..16}; do echo "${genre[i]}|${nums[i]}|${avgR[i]}|${avgD[i]}";done >> ans2_3.txt
+echo "genre|noOfMovies|avgRatings|avgDurations" > ./assets/answers/ans2_3.txt
+for i in {0..16}; do echo "${genre[i]}|${nums[i]}|${avgR[i]}|${avgD[i]}";done >> ./assets/answers/ans2_3.txt
 #Ans-4
-cat imdb.tsv | awk -F'\t' '$1>8 && $5<150 && ($4 ~ /Crime/ || $4 ~ /Comedy/ || $4 ~ /Adventure/){print}' > ans4.txt
+cat imdb.tsv | awk -F'\t' '$1>8 && $5<150 && ($4 ~ /Crime/ || $4 ~ /Comedy/ || $4 ~ /Adventure/){print}' > ./assets/answers/ans4.txt
 
 #Ans-5
-awk -F'\t' '$6 ~ /Tim Robbins/ {print}' imdb.tsv > ans5.txt
+awk -F'\t' '$6 ~ /Tim Robbins/ {print}' imdb.tsv > ./assets/answers/ans5.txt
 
 #Removing_extra_files
 rm avgRatings.txt avgDurations.txt rating.txt duration.txt genreSums.txt
